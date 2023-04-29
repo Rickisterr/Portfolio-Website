ee196b4bc14aa99dbf419c1a1449531bc2b949d1
Rickisterr <sinha.richik@gmail.com>
2023-04-30 02:32:21 +0530

    Home page Content Updates + Typing image animation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,105 @@
 import './Home.css';
 import ParticleEff from '../ParticlesEffect';
+import { useEffect, useState } from 'react';
 
 import Earth_horizon_space from '../Resources/Earth_horizon_space.jpg';
 import STS135_final_flyaround_of_ISS_1 from '../Resources/STS-135_final_flyaround_of_ISS_1 (2).png';
 import STS41B_35_1613_Bruce_McCandless_II from '../Resources/STS41B-35-1613_-_Bruce_McCandless_II_astronaut_floating.png';
+import Laptop from '../Resources/Laptop';
+import Content from '../Content';
 
 
 
 function Home() {
+    var msgval = 0;
+    const [typedmsg, setTypedmsg] = useState('');
+    const msgs = ["Website Design", "Software Development ", "Data Science ", "Artificial Intelligence "];
+    var msgflag = "0"
+
+    function getMsgflag() {
+        if(sessionStorage.getItem("msgflag")) {
+            msgflag = sessionStorage.getItem("msgflag");
+        }
+        else {
+            sessionStorage.setItem("msgflag", "0")
+            msgflag = "0";
+        }
+    }
+
+    function setMsgflag() {
+        sessionStorage.setItem("msgflag", msgflag);
+    }
+
+    function incVal() {
+        if(msgval == 3) {
+            msgval = 0;
+        }
+        else {
+            msgval += 1;
+        }
+    }
+
+    function updateMsg(i) {
+        return(setTypedmsg(msgs[msgval].slice(0, i)));
+    }
+
+    function MessageUpdation() {
+        getMsgflag();
+        if(typedmsg.length == 0) {
+            if(msgflag == "0") {
+                updateMsg(1);
+            }
+            else {
+                msgflag = "0";
+                incVal();
+            }
+            console.log("start");
+        }
+        else if(typedmsg.length == msgs[msgval].length) {
+            msgflag = "1";
+            console.log("Set true");
+            updateMsg(typedmsg.length - 1);
+            console.log("equals deleted");
+        }
+        else {
+            if(msgflag == "1") {
+                updateMsg(typedmsg.length - 1);
+                console.log("deleted");
+            }
+            else {
+                updateMsg(typedmsg.length + 1);
+                console.log("added");
+            }
+        }
+        console.log(msgflag);
+        setMsgflag();
+    };
+
+    useEffect(() => {
+        const interval = setInterval(MessageUpdation, 500);
+        return () => clearInterval(interval);
+    });
 
     return (
         <div className='Wrapper' style={{ height: 'min-content' }}>
             <ParticleEff />
             <div className='ColFlexWrap'>
-                
+                <div className='ViewportPage RowFlexWrap'>
+                    <div>
+                        <h1 className='WhiteNormText'>WELCOME</h1>
+                        <h2 className='WhiteNormText'>MY NAME IS RICHIK SINHA</h2>
+                    </div>
+                    <div>
+                        <Laptop
+                            scale='scale(0.5)'
+                            display={typedmsg}
+                            // display='printf("Per Aspera ad Astra")'
+                        />
+                    </div>
+                </div>
+                <div className='ViewportPage RowFlexWrap'>
+
+                </div>
             </div>
         </div>
     );
---
+++ src/Components/Resources/Laptop.js
@@ -0,0 +1,21 @@
+function Laptop(props) {
+    return (
+        <div style={{ transform: props.scale }}>
+            <svg width="209.24mm" height="148.51mm" version="1.1" viewBox="0 0 209.24 148.51" xmlns="http://www.w3.org/2000/svg">
+                <g transform="translate(-.36276 -58.714)">
+                    <g transform="matrix(1.5976 0 0 1.543 -57.301 -73.884)">
+                        <rect x="38.156" y="85.938" width="124.78" height="85.938" rx="4.125" ry="3.7812" fill="#808080" stroke-width=".26458" />
+                        <g transform="matrix(1 0 0 1.0184 0 -3.3603)">
+                            <rect x="36.094" y="174.62" width="130.97" height="7.5625" rx="6.1875" ry="3.7812" fill="#808080" stroke-width=".26458" />
+                            <rect x="43.312" y="96.594" width="114.81" height="67.719" rx="0" ry="0" stroke-width=".26458" />
+                            <circle cx="100.5" cy="91.5" r="2.5" stroke-width=".32873" />
+                            <text x="45.375" y="103.125" fill="#00ff00" font-size="6px" stroke-width=".26458"><tspan x="45.375" y="103.125">&gt; {props.display}</tspan><tspan x="45.375" y="107.09375" /></text>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    );
+}
+
+export default Laptop;
