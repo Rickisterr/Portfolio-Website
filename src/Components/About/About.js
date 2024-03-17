acd060bb9b14a860d10e6c2bcbbe9a00583981dd
Richik Sinha <richik.backup@gmail.com>
2024-03-17 21:49:13 +0530

    Added Skill Set section to About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,8 @@
 import './About.css';
 import Contact from "../Contact";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 
 import PFP from '../Resources/ProfilePic.jpeg';
+import { Avatar, Typography, Rating } from '@mui/material';
 
 
 function About() {
@@ -21,9 +20,9 @@ function About() {
 
 
 
-                <div className='BoxWrap ColFlexWrap' style={{ gap: '5vmin', marginBottom: '4vmin', paddingTop: '0vmin' }}>
+                <div className='BoxWrap ColFlexWrap' style={{ gap: '20vmin', marginBottom: '4vmin', paddingTop: '0vmin' }}>
 
-
+                    {/* Introduction Box */}
                     <div className="RowFlexWrap" style={{ width: '100%', backgroundColor: '#36454f', gap: '5vmin', paddingBlock: '5vmin', paddingInline: '9vmin', borderStyle: 'solid', borderWidth: '0.25vmin', borderRadius: '1rem', borderColor: '#f4f1de', minHeight: '50vh' }}>
 
                         <div style={{ marginInlineEnd: '2.5vmax' }}>
@@ -52,14 +51,123 @@ function About() {
                     </div>
 
 
+                    {/* Skills */}
+                    <div style={{ width: '100%', paddingInline: '9vmin', paddingBlock: '5vmin', backgroundColor: '#36454f', borderStyle: 'solid', borderWidth: '0.25vmin', borderRadius: '1rem', borderColor: '#f4f1de' }}>
+
+
+                        <h1 className='WhiteNormText Headings' style={{ textAlign: 'left' }}>Skill Set</h1>
+
+
+                        <div className='RowFlexWrap' style={{ gap: '3vmin', marginBlock: '7.5vmin' }}>
+
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>Embedded C</Typography>
+                                    <Rating name="read-only" size='large' value={4.5} precision={0.5} readOnly />
+                                </div>
+                            </div>
+
+
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>Software Development</Typography>
+                                    <Rating name="read-only" size='large' value={4} precision={0.5} readOnly />
+                                </div>
+                            </div>
+
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>Web Development</Typography>
+                                    <Rating name="read-only" size='large' value={4.5} precision={0.5} readOnly />
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+                        <div className='RowFlexWrap' style={{ gap: '3vmin' }}>
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>Python Programming</Typography>
+                                    <Rating name="read-only" size='large' value={5} precision={0.5} readOnly />
+                                </div>
+                            </div>
+
+
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>C/C++ Programming</Typography>
+                                    <Rating name="read-only" size='large' value={4} precision={0.5} readOnly />
+                                </div>
+                            </div>
+
+
+                            <div className='FlexStart' style={{ width: '33%', display: 'flex', flexDirection: 'row', msFlexDirection: 'row', WebkitFlexDirection: 'row', gap: '2vmin', flexGrow: '1' }}>
+                                <div style={{ width: 'max-content' }}>
+                                    <Avatar
+                                        alt="Remy Sharp"
+                                        src={PFP}
+                                        sx={{ width: 100, height: 100 }}
+                                    />
+                                </div>
+                                <div className='ColFlexWrap FlexStart' style={{ gap: '1vmin' }}>
+                                    <Typography component="legend" variant='h6' style={{ color: '#f0ffff' }}>AI/ML</Typography>
+                                    <Rating name="read-only" size='large' value={3.5} precision={0.5} readOnly />
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+
+                    {/* Education */}
+                    
+
+
                 </div>
 
+                {/* Contacts Footer */}
+                <Contact />
 
-            </div>
-
-            {/* Contacts Footer */}
-            <Contact />
-
+            </div >
         </div>
     )
 }
